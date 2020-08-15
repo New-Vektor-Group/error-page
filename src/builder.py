f7bcebe74316a6b39c558c092764bc5fd61f8f16
Sasha <lonagi22@gmail.com>
2020-08-16 00:43:37 +0300

    Update builder.py

--- src/builder.py
+++ src/builder.py
@@ -1,12 +1,13 @@
-with open("template.html","r") as f:
-    t=f.read()
-
 ListErrors=(400,401,402,403,404,405,500,502,503,504,505,526)
 
 for i in ListErrors:
-    t = t.replace("{$TITLE$}", "Error" + str(i))
-    t = t.replace("{$ERROR$}", "Error" + str(i))
-    with open("build/"+str(i)+".html","w") as f:
+    with open("template.html", "r") as f:
+        t = f.read()
+
+    t = t.replace("{$TITLE$}", "Error " + str(i))
+    t = t.replace("{$ERROR$}", "" + str(i))
+
+    with open("../build/"+str(i)+".html","w") as f:
         f.write(t)
 
 print("Done!")
